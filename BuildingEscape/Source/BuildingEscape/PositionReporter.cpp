3cacb7c6ec860fb1b605fb2eeb1aa9de42c4e5a7
JunkieCream <cyriljoker@gmail.com>
2019-01-27 13:36:05 +0300

    BE10 name access

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -9,7 +10,6 @@ UPositionReporter::UPositionReporter()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -18,7 +18,10 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+
+	FString ObjName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position reportrer reporting for %s"), *ObjName);
 	
 }
 
